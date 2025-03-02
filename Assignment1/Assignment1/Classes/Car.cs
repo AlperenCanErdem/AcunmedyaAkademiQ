b73ade81fba27335d7fa03a6d00893d0261be9f4
Alperen Can Erdem <alperencanerdem6@gmail.com>
2025-03-02 18:21:14 +0300

    Assignment completed

---
+++ Assignment1/Assignment1/Classes/Car.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment1.Classes
+{
+    public class Car
+    {
+        public string brand;
+        public string gear;
+
+        public void gearType(string brand, string gear)
+        {
+            Console.WriteLine(brand + " -> " + gear + " viteslidir.");
+        }
+    }
+}
---
+++ Assignment1/Assignment1/Program.cs
@@ -0,0 +1,32 @@
+﻿using Assignment1.Classes;
+
+Mercedes m = new Mercedes();
+m.brand = "Mercedes";
+m.gear = "Otomatik";
+m.gearType(m.brand, m.gear);
+
+Bmw b = new Bmw();
+b.brand = "Bmw";
+b.gear = "Düz";
+b.gearType(b.brand, b.gear);
+
+Audi a = new Audi();
+a.brand = "Audi";
+a.gear = "Otomatik";
+a.gearType(a.brand, a.gear);
+
+Porsche p = new Porsche();
+p.brand = "Porsche";
+p.gear = "Düz";
+p.gearType(p.brand, p.gear);
+
+Toyota t = new Toyota();
+t.brand = "Toyota";
+t.gear = "Düz";
+t.gearType(t.brand, t.gear);
+
+Togg to = new Togg();
+to.brand = "Togg";
+to.gear = "Otomatik";
+to.gearType(to.brand, to.gear);
+
